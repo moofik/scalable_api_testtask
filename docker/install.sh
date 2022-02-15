fd9f0f43a6f527c63f9a3036c93e2011cf72fe0e
moofik <moofik12@gmail.com>
2022-02-15 21:12:39 +0700

    fixes

--- docker/install.sh
+++ docker/install.sh
@@ -2,11 +2,14 @@
 . $(dirname $0)/script/functions.sh
 
 SCRIPT_DIR=$(readlink -e $(dirname $0))
-CONTAINER_NAME=$(docker ps | grep fpm | grep -o "^\w*\b")
 
 docker-compose -f ${SCRIPT_DIR}/develop_stack.yml build
 docker network create --scope=swarm testtask_swarm
 docker stack deploy -c ${SCRIPT_DIR}/develop_stack.yml testtask
+
+sleep 10
+
+CONTAINER_NAME=$(docker ps | grep fpm | grep -o "^\w*\b")
 docker exec -it ${CONTAINER_NAME} bash -c "cd /testtask/backend && composer install"
 docker exec -it ${CONTAINER_NAME} bash -c "cd /testtask/backend && cp .env.dist .env"
 
